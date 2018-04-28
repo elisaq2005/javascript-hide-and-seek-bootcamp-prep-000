14ef2a2c77636a0c5dd94aa7c78c7f2d729b78df
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-28 19:19:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,4 @@ function increaseRankBy(n) {
   }
 }
 
-function deepestChild(nodes) {
-  var nodes = document.querySelectorAll('grand-node div');
-  var mostDeep = nodes[nodes.length - 1];
-  return mostDeep;
-}
+function deepestChild()
