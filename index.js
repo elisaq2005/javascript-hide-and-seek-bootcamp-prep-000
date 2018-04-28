8c4dbd51a0b0bd520bdb5367e3b3284e7efc860e
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-28 19:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   var nodes = document.Selector('#grand-node')
   var deepestNode = grand-node.children[0];
-  fo (var i = 0; i < !deepestNode.childred[i]; )
+  for (var i = 0; i < !deepestNode.childred[i]; i) {
+    deepestNode = 
+  } 
 }
