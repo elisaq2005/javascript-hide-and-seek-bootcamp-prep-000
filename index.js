18143b0b3dfb8cf256ad0110ddc1f1c65ae4e436
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-28 18:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   var rank = document.querySelectorAll('ul.ranked-list li');
-  for (let n = 0; n < ranked-list.length; n++) {
-    rank[n].innerHTML = parseInt(rank[n].innerHTML) + 1;
-   return document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranked-list.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
   }
 }
